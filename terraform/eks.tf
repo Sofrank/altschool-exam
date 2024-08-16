fca6f6819859a9cdd30c8638fd815522fd3af1ff
Sofrank <sofrankibe@gmail.com>
2024-08-16 10:31:29 +0000

    added cluster admin argument

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,26 +2,43 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.23.0"
 
-  cluster_name                   = "${var.project_name}-cluster"
-  cluster_version                = "1.30"
+  cluster_name    = "${var.project_name}-cluster"
+  cluster_version = "1.30"
+
   cluster_endpoint_public_access = true
 
-  enable_irsa = true
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.public_subnets
 
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+    instance_types = ["m5.large"]
+  }
+
   eks_managed_node_groups = {
     altschool-exam-cluster = {
       instance_types = ["t3.large"]
-
-      min_size     = 2
-      max_size     = 6
-      desired_size = 2
-      capacity_type = "SPOT"
+      min_size       = 2
+      max_size       = 5
+      desired_size   = 2
+      capacity_type  = "SPOT"
     }
   }
+
+  enable_cluster_creator_admin_permissions = true
 
   tags = {
     environment = "development"
