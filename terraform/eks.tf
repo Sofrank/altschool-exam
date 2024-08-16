40094d8728a65bea7d6bc5c12f440af2388922f7
Sofrank <sofrankibe@gmail.com>
2024-08-16 06:20:45 +0000

    workflow optimisation

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -12,13 +12,6 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.public_subnets
 
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
-
-    attach_cluster_primary_security_group = true
-  }
-
   eks_managed_node_groups = {
     altschool-exam-cluster = {
       instance_types = ["t3.large"]
